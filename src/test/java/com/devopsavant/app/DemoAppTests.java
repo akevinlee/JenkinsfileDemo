b54437fcbe578c29695ec3298751f5dfdd8fc1e3
akevinlee <kevin.lee@microfocus.com>
2021-01-28 12:01:16 +0000

    Updated Jenkinsfile

--- src/test/java/com/devopsavant/app/DemoAppTests.java
+++ src/test/java/com/devopsavant/app/DemoAppTests.java
@@ -1,10 +1,10 @@
-package com.devopsavant.jenkinsfileenv;
+package com.devopsavant.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JenkinsfileEnvApplicationTests {
+class DemoAppTests {
 
 	@Test
 	void contextLoads() {
